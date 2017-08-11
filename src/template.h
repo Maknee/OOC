8952999ca66b1cffd50850cdb6892fbfcfd3d810
Henry Zhu <henryzhu@seas.upenn.edu>
2017-08-11 10:24:11 -0400

    added a missing macro

--- src/template.h
+++ src/template.h
@@ -122,6 +122,14 @@ typedef struct _String* StringPtr;
 
 #define Vector(type) Vector ## type
 
+
+
+
+
+#define GCC_VERSION (__GNUC__ * 10000 \
+                               + __GNUC_MINOR__ * 100 \
+                               + __GNUC_PATCHLEVEL__)
+
  /* Test for GCC > 4.9.0 */
 #if GCC_VERSION > 40900
 //implement _Generic
