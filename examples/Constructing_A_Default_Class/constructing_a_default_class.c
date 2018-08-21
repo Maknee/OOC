63b311920dc8ceb10203a09939f4b6306b705f11
Henry Zhu <henryzhu@seas.upenn.edu>
2018-08-21 10:37:24 -0400

    Updated Example with comments

--- examples/Constructing_A_Default_Class/constructing_a_default_class.c
+++ examples/Constructing_A_Default_Class/constructing_a_default_class.c
@@ -13,12 +13,25 @@ int main()
 {
 	DefaultExampleStruct default_example_struct = New(DefaultExampleStruct);
 
+	//set the values
+	default_example_struct->a = 1;
+	default_example_struct->b = 2;
+	default_example_struct->c = 3;
+
+	//copy the struct
 	DefaultExampleStruct copy_struct = Call(default_example_struct, copy);
 
+	//make sure that copied struct is the same
+	assert(default_example_struct->a == default_example_struct->a);
+	assert(default_example_struct->b == default_example_struct->b);
+	assert(default_example_struct->c == default_example_struct->c);
+
+	//copy struct calls are valid
 	assert(Call(copy_struct, equals, default_example_struct) == true);
 	assert(Call(copy_struct, compareTo, default_example_struct) == 0);
 	assert(!strcmp(Call(copy_struct, toString), "DefaultExampleStruct"));
 
+	//delete the object
 	Delete(copy_struct);
 	Delete(default_example_struct);
 	return 0;
